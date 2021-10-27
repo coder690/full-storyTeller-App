4d124b306c0b1a7ece45acfd1de9451e8335e055
coder690 <77380137+coder690@users.noreply.github.com>
2021-10-27 18:45:33 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBUFfmyXIdqkwR4j0y1YuPSuizBXwuOvZs",
+    authDomain: "storytellerapp-1d3e7.firebaseapp.com",
+    projectId: "storytellerapp-1d3e7",
+    storageBucket: "storytellerapp-1d3e7.appspot.com",
+    messagingSenderId: "908904875376",
+    appId: "1:908904875376:web:b1251ae92cbd4db0e53bb5"
+  };
